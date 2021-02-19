bdb5ab91e13b76302f52b93cc63f34882cf488ef
Elizab-boop <78388081+Elizab-boop@users.noreply.github.com>
2021-02-19 16:24:44 -0500

    Added LizProject1 to repository.

---
+++ LizProject1/ClassStuff/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizProject1
+{
+    public class Animal
+    {
+        protected string _species;
+        protected string _gender;
+        protected string _diet;
+        protected double _age;
+        protected double _weight;
+
+        public string SummarizeAnimal()
+        {
+            return $"This animal is a(n) {_species}, is {_gender}, {_age} years old, weighs {_weight} pounds, and eats {_diet}.";
+        }
+    }
+}
---
+++ LizProject1/ClassStuff/Mammal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizProject1
+{
+    public class Mammal : Animal
+    {
+        protected bool _hasFur;
+        protected bool _hasHair;
+        protected bool _isTwoLegged;
+        protected bool _canSwim;
+
+        public string SummarizeMammal()
+        {
+            string furMessage;
+
+            if (_hasFur)
+            {
+                furMessage = "has fur";
+            }
+            else if (_hasHair)
+            {
+                furMessage = "has hair";
+            }
+            else
+            {
+                furMessage = "has no hair or fur";
+            }
+
+            string legMessage;
+
+            if (_isTwoLegged)
+            {
+                legMessage = "has two legs";
+            }
+            else
+            {
+                legMessage = "has four legs";
+            }
+
+            string swimMessage;
+
+            if (_canSwim)
+            {
+                swimMessage = "can swim";
+            }
+            else
+            {
+                swimMessage = "cannot swim";
+            }
+
+            return $"This mammal {furMessage}, {legMessage} and {swimMessage}.";
+        }
+
+    }
+}
