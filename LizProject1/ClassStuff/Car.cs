8c524b81bc5ac84fa1339f6655d3c0aaeee7a79f
Elizab-boop <78388081+Elizab-boop@users.noreply.github.com>
2021-02-19 18:19:28 -0500

    Class/Inheritance Practice
    In progress: Added Vehicle/Car and Reptile classes.

---
+++ LizProject1/ClassStuff/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizProject1
+{
+    public class Car : Vehicle
+    {
+        protected string _color;
+        protected string _make;
+        protected string _model;
+        protected int _year;
+
+        public string SummarizeCar()
+        {
+            return $"This car is a(n) {_color} {_make} {_model} and was made in {_year}. ";
+        }
+        public Car(string newColor)
+        {
+            _color = newColor;
+            _make = "Ford";
+            _model = "Fiesta";
+            _year = 2013;
+            _usesGas = true;
+            _fourWheels = true;
+        }
+        public string FullSummaryCar()
+        {
+            return $"{SummarizeVehicle()} {SummarizeCar()}";
+        }
+    }
+
+    
+}
---
+++ LizProject1/ClassStuff/Reptile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizProject1
+{
+    public class Reptile : Animal 
+    {
+        protected bool _egg;
+        protected bool _fourLeg;
+
+
+
+        public string SummarizeReptile()
+        {
+            string eggMessage;
+
+            if (_egg)
+            {
+                eggMessage = "hatches from an egg";
+            }
+            else
+            {
+                eggMessage = "is born live";
+            }
+
+            string legMessage;
+
+            if (_fourLeg)
+            {
+                legMessage = "has four legs";
+            }
+            else
+            {
+                legMessage = "has no legs";
+            }
+
+            return $"This reptile {eggMessage} and {legMessage}.";
+        }
+
+    }
+}
